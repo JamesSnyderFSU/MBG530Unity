5ef0e9ffef0c50f3129491e0243a58350a21d240
Jim Snyder <james.snyder@fullsail.edu>
2014-02-12 09:25:17 -0600

    Final background scroll test
    Scrolling of background textures implemented.

--- GameProject/Assets/Standard Assets/Scripts/scPlayfield.cs
+++ GameProject/Assets/Standard Assets/Scripts/scPlayfield.cs
@@ -3,33 +3,32 @@ using System.Collections;
 
 public class scPlayfield : MonoBehaviour {
 
-	float speedLayerStar = 0.0f;
-	float speedLayerDust = 0.0f;
+	// Set speeds for texture scroll
+
+	float speedLayerStar = 1.0f;
+	float speedLayerDust = 2.0f;
 
 	// Use this for initialization
 	void Start () {
-		/*Vector3 scale = transform.localScale;
-		scale.x = 10f;
-		scale.z = 5f;
-		transform.localScale = scale;
-		Debug.Log ("x" + scale.x + ".z" + scale.z);
 
-		/*float fieldXScale = 3.5f; //Screen.width;
-		float fieldZScale = 7.5f; //Screen.height;
-		transform.localScale = new Vector3(fieldXScale, 1.0f, fieldZScale);*/
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		// Handle scrolling of texture
+		// Offset for texture scrolling
 
 		float offsetLayerStar = (speedLayerStar * Time.time) * -1;
 		float offsetLayerDust = (speedLayerDust * Time.time) * -1;
 
+		// Handle offset of self-illum starfield texture
+
 		renderer.materials[0].SetTextureOffset("_MainTex", new Vector2(0.0f, offsetLayerStar));
-		renderer.materials[0].SetTextureOffset("_BumpMap", new Vector2(0.0f, offsetLayerStar));
+		renderer.materials[0].SetTextureOffset("_Illum", new Vector2(0.0f, offsetLayerStar));
+
+		// Handle offset of transparent/diffuse cloud texture
+
 		renderer.materials[1].SetTextureOffset("_MainTex", new Vector2(0.0f, offsetLayerDust));
-		renderer.materials[1].SetTextureOffset("_BumpMap", new Vector2(0.0f, offsetLayerDust));
+
 	}
 }
